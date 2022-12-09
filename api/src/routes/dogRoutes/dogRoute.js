437f19dc43fa4a2426f48ae07474e9949bba795e
dbandres <dario26081993@outlook.com.ar>
2022-12-09 06:53:11 -0300

    post

--- api/src/routes/dogRoutes/dogRoute.js
+++ api/src/routes/dogRoutes/dogRoute.js
@@ -1,11 +1,12 @@
-const {getApiDogs} = require("../controllers/get")
+const {getApiDogs, getAllDogs, getDbInfo} = require("../controllers/get")
 const { Router } = require('express');
-
+const {Dog, Temperament} = require("../../db")
+const {Op} = require("sequelize")
 const router = Router();
 
 router.get("/", async (req, res, next) =>{
     const {name} = req.query
-    const nameDog = await getApiDogs();
+    const nameDog = await getAllDogs();
     try {
         if(name){
             let dogByName = await nameDog.filter((dog) => dog.name.toLowerCase().includes(name.toLowerCase()))
@@ -13,12 +14,39 @@ router.get("/", async (req, res, next) =>{
             dogByName.length ? res.send(dogByName) : res.status(404).send("Error, Name no existe")
             
         }else{
-        let dogsTotal = await getApiDogs()
+        let dogsTotal = await getAllDogs()
         res.status(200).send(dogsTotal)
         }
     } catch (error) {
         next(error)
     }
+})
+
+router.post("/", async (req, res, next)=>{
+    const {name,height,weight,life,temperament} = req.body
+    try {
+        const newDog = await Dog.create({name,height,weight,life,temperament})
+        
+        res.send("Ha sido creado")
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.use("/temperaments", async (req, res, next)=>{
+    const dogApi = await getApiDogs();
+    let temperApi = dogApi.map((dog) => dog.temperament)
+    //console.log(temperApi)
+    const temperData = temperApi.toString().split(",")
+    console.log(temperData)
+    temperData.forEach(str => {
+        let i = str.trim()
+        Temperament.findOrCreate({
+            where: {name:i}
+        })
+    });
+    const allTemperaments = await Temperament.findAll()
+    res.send(allTemperaments)
 })
 
 module.exports = router;
