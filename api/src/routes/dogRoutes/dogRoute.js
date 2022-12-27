b992f4ae77bec9ddc17d89488a7e32d7e5890d98
Dario Balmaceda <dario26081993@uotlook.com.ar>
2022-12-27 11:14:06 -0300

    cambios get

--- api/src/routes/dogRoutes/dogRoute.js
+++ api/src/routes/dogRoutes/dogRoute.js
@@ -8,13 +8,20 @@ const router = Router();
 router.get("/", async (req, res, next) =>{
     const {name} = req.query
     const nameDog = await getAllDogs();
+    const keys = ["name"]
     try {
         if(name){
-            let namedoguii = await axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${name}`)
-            res.send(namedoguii)
-            //let dogByName = await nameDog.filter(dog => dog.name.toLowerCase().includes(name.toLowerCase()))
-            //console.log(dogByName)
-            //dogByName.length ? res.send(dogByName) : res.status(404).send("Error, Name no existe")
+            const search = (data) =>{
+                return data.filter((item)=>
+                    keys.some((key)=>item[key].toLowerCase().includes(name))
+                )
+            }
+            res.json(search(nameDog))
+            // let namedoguii = await axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${name}`)
+            // res.send(namedoguii)
+            // let dogByName = await nameDog.filter(dog => dog.name.toLowerCase().includes(name.toLowerCase()))
+            // console.log(dogByName)
+            // dogByName.length ? res.send(dogByName) : res.status(404).send("Error, Name no existe")
             
         }else{
         //let dogsTotal = await getAllDogs()
